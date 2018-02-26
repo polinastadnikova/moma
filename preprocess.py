7db1dd5c61a09e685d2a9e2054a5df2d0f8a7087
polina_std <pstadnikova@gmail.com>
2018-02-26 12:20:33 +0100

    add experiments

--- preprocess.py
+++ preprocess.py
@@ -4,88 +4,133 @@ import pandas as pd
 from collections import Counter
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SpanishStemmer, GermanStemmer, EnglishStemmer
-
-stemmer_es = SpanishStemmer()
-stemmer_en = EnglishStemmer()
-stemmer_de = GermanStemmer()
-
 from invert import Matrix
 from scipy.spatial.distance import cosine
-import csv
+#import csv
 
-#define sets of stopwords
-stop_de = set(stopwords.words('german'))
-stop_en = set(stopwords.words('english'))
-stop_es = set(stopwords.words('spanish'))
+#load stemmers
+#stemmer_es = SpanishStemmer()
+#stemmer_en = EnglishStemmer()
+#stemmer_de = GermanStemmer()
 
-#extract mutual concepts
-titles = pd.read_csv('data/mutual.csv', encoding='latin')
-concepts_de = titles['de'].values
-concepts_en = titles['en'].values
-concepts_es = titles['es'].values
+class Embeddings:
+    def __init__(self):
+        # extract mutual concepts
+        titles = pd.read_csv('data/mutual.csv', encoding='latin')
+        self.concepts_de = titles['de'].values
+        self.concepts_en = titles['en'].values
+        self.concepts_es = titles['es'].values
+        self.de_data = None
+        self.en_data = None
+        self.es_data = None
 
+    def get_url(self, lang, concept):
+        return 'https://'+lang+'.wikipedia.org/w/api.php?action=query&prop=extracts&rvprop=content&format=json&titles='+concept
 
-def get_url(lang, concept):
-    return 'https://'+lang+'.wikipedia.org/w/api.php?action=query&prop=extracts&rvprop=content&format=json&titles='+concept
-
-def get_info(url):
-    response = requests.get(url)
-    #print(str(response.content))
-    if response.status_code == 200:
-        return json.loads(response.content.decode())
-    else:
-        return None
-
-def get_text(js, lang):
-    article = next(iter(js['query']['pages'].values()))
-    text = (article['extract'])
-    text = [t.lower() for t in text.split() if t.isalpha()]
-    if lang == 'de':
-        text = [stemmer_de.stem(t) for t in text if t not in stop_de]
-    elif lang == 'en':
-        text = [stemmer_en.stem(t) for t in text if t not in stop_en]
-    else:
-        text = [stemmer_es.stem(t) for t in text if t not in stop_es]
-    return text
-
-def build_corpus(concepts, lang):
-    corpus = dict()
-    for i in range(len(concepts)):
-        url = get_url(lang, concepts[i])
-        content = get_info(url)
-        if concepts[i] in corpus.keys():
-            corpus[concepts[i]] += get_text(content,lang)
+    def get_info(self,url):
+        response = requests.get(url)
+        #print(str(response.content))
+        if response.status_code == 200:
+            return json.loads(response.content.decode())
         else:
-            corpus[concepts[i]] = get_text(content, lang)
-    return corpus
+            return None
 
+    def get_text(self,js, lang):
+        #define sets of stopwords
+        stop_de = set(stopwords.words('german'))
+        stop_en = set(stopwords.words('english'))
+        stop_es = set(stopwords.words('spanish'))
+
+        article = next(iter(js['query']['pages'].values()))
+        text = (article['extract'])
+        text = [t.lower() for t in text.split() if t.isalpha()]
+        if lang == 'de':
+            #text = [stemmer_de.stem(t) for t in text if t not in stop_de]
+            text = [t for t in text if t not in stop_de]
+        elif lang == 'en':
+            #text = [stemmer_en.stem(t) for t in text if t not in stop_en]
+            text = [t for t in text if t not in stop_en]
+        else:
+            #text = [stemmer_es.stem(t) for t in text if t not in stop_es]
+            text = [t for t in text if t not in stop_es]
+        return text
+
+    def build_corpus(self,concepts, lang):
+        corpus = dict()
+        for i in range(len(concepts)):
+            url = self.get_url(lang, concepts[i])
+            content = self.get_info(url)
+            if concepts[i] in corpus.keys():
+                corpus[concepts[i]] += self.get_text(content,lang)
+            else:
+                corpus[concepts[i]] = self.get_text(content, lang)
+        return corpus
+
+    def train(self):
+        self.de_data = Matrix(self.build_corpus(self.concepts_de,'de'),self.concepts_de)
+        self.de_data.fill_matrix()
+
+        self.en_data = Matrix(self.build_corpus(self.concepts_en,'en'),self.concepts_en)
+        self.en_data.fill_matrix()
+
+        self.es_data = Matrix(self.build_corpus(self.concepts_es,'es'),self.concepts_es)
+        self.es_data.fill_matrix()
 
 
 '''Experiment 1'''
-result1 = open('experiment1.csv','a', encoding='utf-8')
-
-de_data = Matrix(build_corpus(concepts_de,'de'),concepts_de)
-de_data.fill_matrix()
-
-en_data = Matrix(build_corpus(concepts_en,'en'),concepts_en)
-en_data.fill_matrix()
-
-es_data = Matrix(build_corpus(concepts_es,'es'),concepts_es)
-es_data.fill_matrix()
-
-for n in range(len(concepts_de)):
-    result1.write(concepts_de[n]+'\t'+concepts_en[n]+'\t'+concepts_es[n]+'\n')
-    words_de = [w[0] for w in de_data.get_words(n)]
-    words_en = [w[0] for w in en_data.get_words(n)]
-    words_es = [w[0] for w in es_data.get_words(n)]
-    for k in range(len(words_de)):
-        result1.write(words_de[k]+'\t'+words_en[k]+'\t'+words_es[k]+'\n')
-    result1.write('\n')
+def compare_concepts(concepts_de, concepts_en, concepts_es, de_data, en_data, es_data):
+    result1 = open('experiment1.csv','w', encoding='utf-8')
+    for n in range(len(concepts_de)):
+        result1.write(concepts_de[n]+'\t'+concepts_en[n]+'\t'+concepts_es[n]+'\n')
+        result1.write('\n')
+        words_de = [w[0] for w in de_data.get_words(n)]
+        words_en = [w[0] for w in en_data.get_words(n)]
+        words_es = [w[0] for w in es_data.get_words(n)]
+        for k in range(len(words_de)):
+            result1.write(words_de[k]+'\t'+words_en[k]+'\t'+words_es[k]+'\n')
+        result1.write('\n')
+        result1.write('\n')
     #fields = [concepts_de[n], concepts_en[n],concepts_es[n]]
     #writer = csv.DictWriter(result1, fieldnames=fields)
     #writer.writeheader()
     #writer.writerow(results)
 
+'''Experiment 2'''
+
+#for two words of the same language
+def compare_same(word1, word2, data):
+    w1=list(data.voc).index(word1)
+    w2=list(data.voc).index(word2)
+    return cosine(data.matrix[w1,:], data.matrix[w2,:])
+
+if __name__ == '__main__':
+    print ('Loading embeddings...')
+    emb=Embeddings()
+    emb.train()
+    
+    exp_number=input('Which experiment do you choose? ')
+    if exp_number == '1':
+        print ('Comparing concepts...')
+        compare_concepts(emb.concepts_de,emb.concepts_en,emb.concepts_es, emb.de_data, emb.en_data, emb.es_data)
+        print('Done. You can find the result in "experiment1.csv"')
+    elif exp_number == '2':
+        print ('Comparing two words of the same language...')
+        proceed = True
+        while proceed:
+            lang = input('Give the language (en, de, es): ')
+            word1 = input('Give the first word: ')
+            word2 = input('Give the second word: ')
+            if lang == 'en':
+                print(compare_same(word1, word2, emb.en_data))
+            elif lang == 'de':
+                print(compare_same(word1, word2,emb.de_data))
+            else:
+                print(compare_same(word1, word2, emb.es_data))
+            next = input('Do you want to proceed with this experiment? ')
+            if next == 'yes':
+                continue
+            else:
+                proceed = False
 
 
 
